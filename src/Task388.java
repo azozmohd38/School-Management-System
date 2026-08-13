3641c20052578aeb2bbc11e91b5076ab6bf66e9c
Abdulaziz <azozmohd38@gmail.com>
2026-08-13 08:41:20 +0400

    add task388(Print a String in Reverse (One Character at a Time)

---
+++ src/Task388.java
@@ -0,0 +1,20 @@
+public class Task388 {
+    public static void printReverse(String str) {
+
+        if (str.isEmpty()) {
+            return;
+        }
+
+
+        printReverse(str.substring(1));
+
+
+        System.out.println(str.charAt(0));
+    }
+
+    public static void main(String[] args) {
+        printReverse("cat");
+        IO.println("---");
+        printReverse("hello");
+    }
+}
