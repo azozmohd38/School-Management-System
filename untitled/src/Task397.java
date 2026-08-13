bc049bcbe62907d8c8ef83e9ff2338577d5474c7
Abdulaziz <azozmohd38@gmail.com>
2026-08-13 10:44:25 +0400

    add task 397(Convert a Number to Binary (String))

---
+++ untitled/src/Task397.java
@@ -0,0 +1,17 @@
+public class Task397 {
+    public static String toBinary(int n) {
+        // Base cases: 0 or 1
+        if (n == 0) return "0";
+        if (n == 1) return "1";
+
+        
+        return toBinary(n / 2) + (n % 2);
+    }
+
+    public static void main(String[] args) {
+        IO.println(toBinary(2));
+        IO.println(toBinary(5));
+        IO.println(toBinary(10));
+        IO.println(toBinary(1));
+    }
+}
