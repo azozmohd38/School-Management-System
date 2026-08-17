9dd3989d0ad595ee45b8731a8caa25779e128579
Abdulaziz <azozmohd38@gmail.com>
2026-08-17 09:59:00 +0400

    started working in this project

---
+++ STUDY/src/STUDY.java
@@ -0,0 +1,9 @@
+public class STUDY {
+    public static void main(String[]args) {
+
+       // IO.println("Hello");
+       byte binaryToDecimal=0b657657;
+       IO.println(binaryToDecimal);
+    }
+}
+
---
+++ SchoolManagementSystem/src/entities/Teacher.java
@@ -0,0 +1,5 @@
+package entities;
+
+import interfaces.Displayable;
+
+
