34fc7038290b2072f7b635bcf2c35c7ce9895b3f
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:05:12 +0400

    refactor(interfaces): keep Searchable aligned to required contract [Task 2.4]

--- SchoolManagementSystem/src/interfaces/Searchable.java
+++ SchoolManagementSystem/src/interfaces/Searchable.java
@@ -5,11 +5,4 @@ public interface Searchable {
     Object[] search(String keyword);
 
     Object searchById(String id);
-
-    boolean existsById(String id);
-
-    default boolean hasMatches(String keyword) {
-        Object[] results = search(keyword);
-        return results != null && results.length > 0;
-    }
 }
